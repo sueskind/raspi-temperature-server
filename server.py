1ba622c0f5bf63c970b807e7a882afc64432861c
sueskind <52210599+sueskind@users.noreply.github.com>
2021-03-30 23:50:19 +0200

    Prototype with dummy data

--- server.py
+++ server.py
@@ -1,3 +1,5 @@
+import datetime as dt
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -11,7 +13,10 @@ def index():
 @app.route("/data")
 def get_data():
     import random
-    return {"temperatures": [random.random() * 3 + 30 for _ in range(20)]}
+    return {
+        "times": [dt.datetime(2020, 3, 3, 12, i, random.randint(0, 59)).isoformat() for i in range(3, 10)],
+        "sensors": [[random.random() * 5 + 30 for _ in range(3, 10)] for _ in range(3)]
+    }
 
 
 if __name__ == '__main__':
