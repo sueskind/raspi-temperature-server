3eb14fa40b7c850b32afaed090069483c86f3fa2
sueskind <52210599+sueskind@users.noreply.github.com>
2021-03-30 20:51:48 +0200

    Simple data API

--- server.py
+++ server.py
@@ -8,5 +8,11 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/data")
+def get_data():
+    import random
+    return {"temperatures": [random.random() * 3 + 30 for _ in range(20)]}
+
+
 if __name__ == '__main__':
     app.run(debug=True)
